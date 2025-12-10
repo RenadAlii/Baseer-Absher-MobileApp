8f1e0ed36fb77b0a9d091e9a340b31e99c11f096
Renad . <ren4dali@gmail.com>
2025-12-10 16:28:12 +0300

    feat: Add android map " not working"

--- composeApp/src/androidMain/kotlin/com/baseer/baseer/presentation/location/screen/NativeMapView.android.kt
+++ composeApp/src/androidMain/kotlin/com/baseer/baseer/presentation/location/screen/NativeMapView.android.kt
@@ -3,12 +3,15 @@ package com.baseer.baseer.presentation.location.screen
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.baseer.baseer.domain.model.LocationData
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
@@ -19,38 +22,44 @@ actual fun NativeMapView(
     selectedLocation: LocationData?,
     onMapClick: (latitude: Double, longitude: Double) -> Unit
 ) {
+    val defaultLocation = LatLng(
+        selectedLocation?.latitude ?: 30.0,
+        selectedLocation?.longitude ?: 30.0
+    )
+    val defaultZoom = 14f
+
     val cameraPositionState = rememberCameraPositionState {
-        selectedLocation?.let {
-            position = CameraPosition.fromLatLngZoom(
-                LatLng(it.latitude, it.longitude),
-                15f
-            )
-        }
+        position = CameraPosition.fromLatLngZoom(defaultLocation, defaultZoom)
     }
 
+    val markerState = remember { MarkerState(position = defaultLocation) }
+
     LaunchedEffect(selectedLocation) {
-        selectedLocation?.let {
+        selectedLocation?.let { location ->
+            val newLatLng = LatLng(location.latitude, location.longitude)
             cameraPositionState.animate(
-                CameraUpdateFactory.newLatLngZoom(
-                    LatLng(it.latitude, it.longitude),
-                    15f
-                )
+                update = CameraUpdateFactory.newLatLngZoom(newLatLng, defaultZoom),
+                durationMs = 500
             )
+            markerState.position = newLatLng
         }
     }
 
     GoogleMap(
         modifier = modifier.fillMaxSize(),
         cameraPositionState = cameraPositionState,
+        uiSettings = MapUiSettings(zoomControlsEnabled = false, myLocationButtonEnabled = false),
+        properties = MapProperties(isMyLocationEnabled = true),
+
         onMapClick = { latLng ->
+            onMapClick(latLng.latitude, latLng.longitude)
+        },
+        onMapLongClick = { latLng ->
             onMapClick(latLng.latitude, latLng.longitude)
         }
     ) {
-        selectedLocation?.let { location ->
-            Marker(
-                state = MarkerState(position = LatLng(location.latitude, location.longitude)),
-                title = location.address
-            )
-        }
+        Marker(
+            state = markerState
+        )
     }
 }
